a1273e6f59a5b32d807a3d0420aadeeed2709cd6
IshaFaodail <ishakunwarrao@gmail.com>
2025-07-27 12:16:41 +0530

    data transformation is done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,7 +8,7 @@ from src import logger
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation, DataTransformatinoConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -20,7 +20,7 @@ class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
     
-    def initiate_data_initiate(self):
+    def initiate_data_ingestion(self):
         logger.logging.info('Entered the data ingestion method or component')
         try:
             df = pd.read_csv('data\stud.csv')
@@ -42,3 +42,8 @@ class DataIngestion:
         except Exception as e:
             raise exception.CustomException(e, sys)
         
+if __name__ == '__main__':
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
